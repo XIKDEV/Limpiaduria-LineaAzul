1bd88b625a4ab4d80ff79d681d495f242f6f04bb
Alderick Hernán Rios Contreras <93097571+XIKDEV@users.noreply.github.com>
2022-09-11 13:25:05 -0700

    conexion BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST_BD,
+      port: Number(process.env.PORT_BD),
+      username: process.env.USER_BD,
+      password: process.env.PASSWORD_BD,
+      database: process.env.BD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
